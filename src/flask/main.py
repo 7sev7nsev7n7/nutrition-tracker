63d865bc9778f98b9af0b43e79d3e1c53fcba2ac
sev7nsev7n7 <sev7nse7en@gmail.com>
2024-08-04 18:41:35 -0600

    Added user cookies for login

--- src/flask/main.py
+++ src/flask/main.py
@@ -6,13 +6,14 @@ main program to run and execute flask site
 
 from flask import Flask, render_template, request, redirect, make_response
 from login import login
+from register import register
 app = Flask(__name__,static_url_path='')
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET']) # main page
 def index():
     return render_template("index.html")
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST']) # login http post
 def userLogin():
     if request.method == 'POST':
         username = request.form['username']
@@ -20,15 +21,32 @@ def userLogin():
 
         if login(username, password):
             print("success!")
-            resp = make_response("Success")
-            resp.set_cookie('username',username)
-            return redirect('/home')
+            resp = make_response(redirect('/home'))
+            resp.set_cookie('username',value=username)
+            print(request.cookies.get('user'))
+            return resp
         else:
             return redirect('/')
 
-@app.route("/home")
+@app.route('/register', methods=['GET']) # registration page
+def userRegister():
+    return render_template("register.html")
+
+@app.route('/postregister',methods=['POST']) # registration http post
+def postRegister():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if register(username, password):
+            print("success!")
+            return redirect('/')
+        else:
+            return redirect('/register')
+    pass
+
+@app.route("/home") # user home page
 def home():
-    return render_template("home.html", user = request.cookies.get('username'))
+    return render_template("home.html", username = request.cookies.get('username'))
 
 if __name__ == "__main__":
     app.run()
