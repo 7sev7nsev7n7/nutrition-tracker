c9b06eeb932b79c87e5add332b188817c67ad407
sev7nsev7n7 <sev7nse7en@gmail.com>
2024-08-04 18:15:21 -0600

    Updated user login landing page

--- src/flask/main.py
+++ src/flask/main.py
@@ -4,27 +4,31 @@
 main program to run and execute flask site
 '''
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 from login import login
 app = Flask(__name__,static_url_path='')
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['POST'])
 def userLogin():
-    username = request.form['username']
-    password = request.form['password']
-    if login(username, password):
-        print("success!")
-        return redirect('/home')
-    else:
-        return redirect('/')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if login(username, password):
+            print("success!")
+            resp = make_response("Success")
+            resp.set_cookie('username',username)
+            return redirect('/home')
+        else:
+            return redirect('/')
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user = request.cookies.get('username'))
 
 if __name__ == "__main__":
     app.run()
