b758526bfffe66b9f372ba626042959af1f185fc
sev7nsev7n7 <sev7nse7en@gmail.com>
2024-08-06 10:43:37 -0600

    Added error handling for 404, along with custom error page

--- src/flask/main.py
+++ src/flask/main.py
@@ -100,5 +100,9 @@ def updateinfo():
             updateData(request.cookies.get('username'),**new)
     return redirect('/home')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('/notfound.html'), 404
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
